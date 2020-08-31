f99b825fea24a17570525e8c69c1a2fbb1cdf77f
Izaias Cintra <izaiasacintra@gmail.com>
2020-08-31 12:34:48 -0300

    DepartmentList view design

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,6 +9,8 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -18,7 +20,7 @@ public class Main extends Application {
 			scroollPane.setFitToHeight(true);
 			scroollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scroollPane);
+			mainScene = new Scene(scroollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
@@ -27,6 +29,10 @@ public class Main extends Application {
 		}
 	}
 
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
