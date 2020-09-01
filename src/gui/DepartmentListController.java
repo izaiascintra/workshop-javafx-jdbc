1b83ef67ab1aab4427b3a19896af9359c176c340
Izaias Cintra <izaiasacintra@gmail.com>
2020-09-01 15:33:57 -0300

    Department ComboBox

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -110,6 +110,7 @@ public class DepartmentListController implements Initializable, DataChangeListen
 			dialogStage.initModality(Modality.WINDOW_MODAL);
 			dialogStage.showAndWait();
 		} catch (IOException e) {
+			e.printStackTrace();
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
 	}
@@ -172,4 +173,4 @@ public class DepartmentListController implements Initializable, DataChangeListen
 			}
 		}
 	}
-}
+}	
